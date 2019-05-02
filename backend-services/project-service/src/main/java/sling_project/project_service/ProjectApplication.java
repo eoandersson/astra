b5f2396b2ee170c4d7f9a5d204e1776029d1b877
elila927 <elila927@su03-103.ad.liu.se>
2019-05-02 11:34:20 +0200

    Add Eureka Server and Clients for service registration

--- backend-services/project-service/src/main/java/sling_project/project_service/ProjectApplication.java
+++ backend-services/project-service/src/main/java/sling_project/project_service/ProjectApplication.java
@@ -2,9 +2,11 @@ package sling_project.project_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import sling_project.project_service.ProjectApplication;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProjectApplication 
 {
