0ceb669ea5c97f2985c1c0d411d4bd6baab53b16
Erik Andersson <erik.osc.andersson@gmail.com>
2019-06-12 20:21:32 +0200

    Move project variables to a .env file
    Several variables are used in several places in
    the repository. Moving these variables to a .env
    file in the project's root directory will allow for
    easier modification of these values.

--- project-service/src/main/java/sling_project/project_service/security/SecurityConstants.java
+++ project-service/src/main/java/sling_project/project_service/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package sling_project.project_service.security;
 
 public class SecurityConstants {
-	public static final String SECRET = ***REMOVED***;
+	public static final String SECRET = System.getenv("JWT_SECRET");
 	public static final long EXPIRATION_TIME = 86400000;
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
