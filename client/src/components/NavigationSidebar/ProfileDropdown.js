a968560931bebea9b6b322008d54a10a38d7d7c6
Erik Andersson <erik.osc.andersson@gmail.com>
2019-07-06 22:20:09 +0200

    Add profile dropdown in sidebar

---
+++ client/src/components/NavigationSidebar/ProfileDropdown.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import store from "../../store";
+import { userSignOut } from "../../actions";
+
+import { Dropdown, Icon } from "semantic-ui-react";
+
+class ProfileDropdown extends Component {
+  logout = () => {
+    localStorage.removeItem("JWT");
+    store.dispatch(userSignOut());
+    this.props.history.push("/");
+  };
+
+  render() {
+    return (
+      <Dropdown
+        trigger={
+          <Icon className="navbar-icon" name="user circle outline" size="big" />
+        }
+        icon={null}
+      >
+        <Dropdown.Menu>
+          <Dropdown.Item icon="user outline" text="Profile" />
+          <Dropdown.Item icon="cog" text="Settings" />
+          <Dropdown.Divider />
+          <Dropdown.Item
+            icon="logout"
+            text="Log Out"
+            onClick={() => this.logout()}
+          />
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  }
+}
+
+export default withRouter(ProfileDropdown);
