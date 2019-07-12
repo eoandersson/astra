0ad2eec97294c831c58da6cdbb2cd4a6bfd8c155
Erik Andersson <erik.osc.andersson@gmail.com>
2019-07-12 20:43:25 +0200

    Redesign landing/login/register page

--- client/src/views/LoginPage/LoginForm.js
+++ client/src/views/LoginPage/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import store from "../../store";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Message, Icon } from "semantic-ui-react";
 import login from "../../data/Login";
 
 class LoginForm extends Component {
@@ -11,7 +11,8 @@ class LoginForm extends Component {
     this.state = {
       username: "",
       password: "",
-      isLoading: store.getState().loading.loginLoading
+      isLoading: store.getState().loading.loginLoading,
+      loginError: false
     };
 
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
@@ -39,30 +40,50 @@ class LoginForm extends Component {
     this.setState({ password: event.target.value });
   }
 
-  handleLogin(event) {
+  handleDismiss = () => {
+    this.setState({
+      loginError: false
+    });
+  };
+
+  async handleLogin(event) {
     event.preventDefault();
     const { username, password } = this.state;
     const { history } = this.props;
-    login(username, password, history);
+    const success = await login(username, password);
+    if (success) history.push("/home");
+    else {
+      this.setState({
+        loginError: true
+      });
+    }
   }
 
   render() {
     const { username, password, isLoading } = this.state;
+    const { loginError } = this.state;
     return (
       <div className="login-form">
-        <Form>
+        <h3>Sign in to Astra</h3>
+        <Form error={loginError} inverted>
           <Form.Field>
-            <label>Username</label>
+            <label>
+              <Icon name="user" />
+              Username
+            </label>
             <input
               type="text"
-              placeholder="Enter username"
+              placeholder="Username"
               value={username}
               onChange={this.handleUsernameChange}
             />
           </Form.Field>
 
           <Form.Field>
-            <label>Password</label>
+            <label>
+              <Icon name="key" />
+              Password
+            </label>
             <input
               type="password"
               placeholder="Password"
@@ -70,16 +91,26 @@ class LoginForm extends Component {
               onChange={this.handlePasswordChange}
             />
           </Form.Field>
+          <Message
+            error
+            header="Login Failed"
+            content="Incorrect username or password."
+            onDismiss={this.handleDismiss}
+          />
           <Button
             positive
             type="submit"
             onClick={this.handleLogin}
             loading={isLoading}
             disabled={isLoading}
+            style={{ width: "100%" }}
           >
             Sign In
           </Button>
         </Form>
+        <div className="register-link" style={{ marginTop: "10px" }}>
+          <Link to="/register">Not a user? Register here!</Link>
+        </div>
       </div>
     );
   }
