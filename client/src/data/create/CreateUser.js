9191d039931100ce61ff0ad8297bfa884f6f2a95
Erik Andersson <erik.osc.andersson@gmail.com>
2019-07-12 20:44:54 +0200

    Refactor login and register api calls

--- client/src/data/create/CreateUser.js
+++ client/src/data/create/CreateUser.js
@@ -1,22 +1,25 @@
 import store from "../../store";
 import { setRegisterLoading, setRegisterFinished } from "../../actions";
 
-export default function createUser({ username, password, history }) {
+export default async function createUser({ username, password }) {
   store.dispatch(setRegisterLoading());
-  fetch("/login-service/users/register", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password
-    })
-  }).then(response => {
-    store.dispatch(setRegisterFinished());
-    if (response.status === 200) {
-      history.push("/");
-    }
-  });
+  let response = null;
+  try {
+    response = await fetch("/login-service/users/register", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    });
+  } catch (err) {
+    return err;
+  }
+  store.dispatch(setRegisterFinished());
+  if (!response) return 400;
+  return response.status;
 }
