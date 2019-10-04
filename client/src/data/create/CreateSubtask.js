db26e59b150558b02010238dfdbf37a47e44ae23
Erik Andersson <erik.osc.andersson@gmail.com>
2019-10-04 09:05:31 +0200

    Update task/subtask identification
    Changed the task and subtask identification
    to use a generated ObjectId rather than the
    task/subtask's name. This way tasks and subtasks
    can have the same name without any issues.

--- client/src/data/create/CreateSubtask.js
+++ client/src/data/create/CreateSubtask.js
@@ -1,13 +1,33 @@
 import { store } from "../../store";
 import { addSubtask } from "../../actions";
 
+const parseObjectId = mongoId => {
+  var result =
+    pad0(mongoId.timestamp.toString(16), 8) +
+    pad0(mongoId.machineIdentifier.toString(16), 6) +
+    pad0(mongoId.processIdentifier.toString(16), 4) +
+    pad0(mongoId.counter.toString(16), 6);
+
+  return result;
+};
+
+const pad0 = (str, len) => {
+  var zeros = "00000000000000000000000000";
+  if (str.length < len) {
+    return zeros.substr(0, len - str.length) + str;
+  }
+
+  return str;
+};
+
 export default function createSubtask({
   project,
   projectId,
-  taskName,
+  taskId,
   subtask,
   category
 }) {
+  console.log(taskId);
   fetch("/project-service/projects/subtask", {
     method: "POST",
     headers: {
@@ -17,28 +37,33 @@ export default function createSubtask({
     },
     body: JSON.stringify({
       projectId: projectId,
-      taskName,
+      taskId: parseObjectId(taskId),
       subtask: {
         name: subtask.name,
         description: subtask.description,
         status: 0
       }
     })
-  }).then(response => {
-    if (response.status === 200) {
+  })
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong ...");
+      }
+    })
+    .then(subtaskResponse => {
       var payload = {
         project: project,
-        name: taskName,
+        taskId,
         subtask: {
-          name: subtask.name,
-          description: subtask.description,
-          status: 0
+          subtaskId: subtaskResponse.subtaskId,
+          name: subtaskResponse.name,
+          description: subtaskResponse.description,
+          status: subtaskResponse.status
         },
-        category
+        category: category
       };
       store.dispatch(addSubtask(payload));
-    } else {
-      console.log("Error");
-    }
-  });
+    });
 }
--- client/src/data/update/UpdateSubtask.js
+++ client/src/data/update/UpdateSubtask.js
@@ -1,8 +1,27 @@
 import { store } from "../../store";
 import { editSubtask } from "../../actions";
 
+const parseObjectId = mongoId => {
+  var result =
+    pad0(mongoId.timestamp.toString(16), 8) +
+    pad0(mongoId.machineIdentifier.toString(16), 6) +
+    pad0(mongoId.processIdentifier.toString(16), 4) +
+    pad0(mongoId.counter.toString(16), 6);
+
+  return result;
+};
+
+const pad0 = (str, len) => {
+  var zeros = "00000000000000000000000000";
+  if (str.length < len) {
+    return zeros.substr(0, len - str.length) + str;
+  }
+
+  return str;
+};
+
 export default function updateSubtask(input) {
-  const { project, projectId, taskName, subtask, status, category } = input;
+  const { project, projectId, taskId, subtask, status, category } = input;
   fetch("/project-service/projects/subtask", {
     method: "PUT",
     headers: {
@@ -12,21 +31,26 @@ export default function updateSubtask(input) {
     },
     body: JSON.stringify({
       projectId: projectId,
-      taskName: taskName,
+      taskId: parseObjectId(taskId),
       subtask: {
+        subtaskId: parseObjectId(subtask.subtaskId),
         name: subtask.name,
         description: subtask.description,
         status: status
       }
     })
   }).then(response => {
-    console.log(response.status);
     if (response.status === 200) {
       var payload = {
         project: project,
-        name: taskName,
-        subtaskName: subtask.name,
-        status: status,
+        taskId,
+        subtaskId: subtask.subtaskId,
+        subtask: {
+          subtaskId: subtask.subtaskId,
+          name: subtask.name,
+          description: subtask.description,
+          status: status
+        },
         category: category
       };
       store.dispatch(editSubtask(payload));
